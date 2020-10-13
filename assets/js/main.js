3eed416b4d1698aee2acc6c0bbef1613381e7aac
RafsanBabu <rafsan.cse.ru13@GMAIL.COM>
2020-10-13 00:22:14 -0700

    our partner banner added

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,24 @@ for(let i=0; i<tabPanes.length;i++){
         
     })
 }
+
+
+/* owl carousel training member */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+      items:5,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:2000,
+      autoplayHoverPause:true
+  });
+  $('.play').on('click',function(){
+      owl.trigger('play.owl.autoplay',[1000])
+  })
+  $('.stop').on('click',function(){
+      owl.trigger('stop.owl.autoplay')
+  })
